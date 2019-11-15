7b3aaf227e2ff6e26f6946113c52d996012cba06
Boon Han <cnboonhan94@gmail.com>
2019-11-15 18:13:41 +0800

    pretty print ros monitor

--- tools/ros_monitor.sh
+++ tools/ros_monitor.sh
@@ -7,5 +7,5 @@ PROJECT_HOME=$(pwd) #TODO: Change to export from config file
 ROS1_SETUP=/opt/ros/melodic/setup.bash
 ROS2_SETUP=/opt/ros/dashing/setup.bash
 
-gnome-terminal -x bash -c "source $ROS1_SETUP; watch rostopic list; exec $SHELL"
-gnome-terminal -x bash -c "source $ROS2_SETUP; watch ros2 topic list; exec $SHELL"
+gnome-terminal -- bash -c "source $ROS1_SETUP; watch 'rostopic list | pr --columns=2'; exec $SHELL"
+gnome-terminal -- bash -c "source $ROS2_SETUP; watch 'ros2 topic list | pr --columns=2'; exec $SHELL"
